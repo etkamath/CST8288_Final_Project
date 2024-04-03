c463217d0c08a3f8955a011191ce2e2b17a43d65
Etka Dakak <etkadakak12@gmail.com>
2024-04-03 02:39:54 -0400

    Cont for Servlet

--- CST8288FinalProject/src/main/java/net/javaguides/registration/dao/EmployeeDao.java
+++ CST8288FinalProject/src/main/java/net/javaguides/registration/dao/EmployeeDao.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import net.javaguides.employeemanagement.model.Employee;
+
+import net.javaguides.registration.model.Employee;
 
 public class EmployeeDao {
 
@@ -19,13 +20,13 @@ public class EmployeeDao {
         Class.forName("com.mysql.jdbc.Driver");
 
         try (Connection connection = DriverManager
-            .getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "root", "root");
+            .getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSLlse", "root=fa", "root");
 
             // Step 2:Create a statement using connection object
             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
             preparedStatement.setInt(1, 1);
-            preparedStatement.setString(2, employee.getFirstName());
-            preparedStatement.setString(3, employee.getLastName());
+            preparedStatement.setString(2, employee.getFirstname());
+            preparedStatement.setString(3, employee.getLastname());
             preparedStatement.setString(4, employee.getUsername());
             preparedStatement.setString(5, employee.getPassword());
             preparedStatement.setString(6, employee.getAddress());
