573de4aefcee4980b85c5ab583dd082af7ecc462
Etka Dakak <etkadakak12@gmail.com>
2024-04-07 19:09:57 -0400

    Correct Sql Build for Fproject (DO NOT MERGE WITHOUT MY CONSENT WE ARE WORKING)

--- src/app/model/CharitableOrganization.java
+++
@@ -1,7 +0,0 @@
-package app.model;
-
-public class CharitableOrganization extends User{
-    public CharitableOrganization(String name, String email, String password) {
-        super(name, email, password, UserType.CHARITABLE_ORGANIZATIONS);
-    }
-}
--- src/app/model/Retailer.java
+++
@@ -1,7 +0,0 @@
-package app.model;
-
-public class Retailer extends User{
-    public Retailer(String name, String email, String password) {
-        super(name, email, password, UserType.RETAILERS);
-    }
-}
